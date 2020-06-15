696d1e6386bb8312cafa8440ff146594852faabe
HEINRICK7 <heinrickcostta@gmail.com>
2020-06-15 16:17:40 -0300

    chore: update

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
