4b477d76c0a936b68b0f4a30d466396322edb34f
HEINRICK7 <heinrickcostta@gmail.com>
2020-07-20 17:03:07 -0300

    feat: ensure SignUpController returns 500 if Emailvalidator throws

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,7 @@
 import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing.param.error'
 import { InvalidParamError } from '../errors/invalid.param.error'
+import { ServerError } from '../errors/server.error'
 import { EmailValidator } from '../protocols/emailValidator'
 
 interface SutTypes {
@@ -93,6 +94,27 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+  })
+
+  test('Should return 500 if EmailValidator throws', () => {
+    class EmailValidatorStub implements EmailValidator {
+      isValid (email: string): boolean {
+        throw new Error()
+      }
+    }
+    const emailValidatorStub = new EmailValidatorStub()
+    const sut = new SignUpController(emailValidatorStub)
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 
   test('Should call EmailValidator with correct email', () => {
