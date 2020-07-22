f932c55a74917420d35c2b7b6dc3743bc2511b98
HEINRICK7 <heinrickcostta@gmail.com>
2020-07-22 08:17:09 -0300

    refactor: movr sucess http response to ahelper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -5,7 +5,13 @@ export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
+
 export const serverError = (): HttpResponse => ({
   statusCode: 500,
   body: new ServerError()
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
 })
