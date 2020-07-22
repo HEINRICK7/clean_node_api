fcc0ba8d8cc3f36c4d1d59701d60df21f01349c6
HEINRICK7 <heinrickcostta@gmail.com>
2020-07-22 17:15:49 -0300

    refactor: move Sut creation to a factory helper method

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,8 +1,7 @@
 import {AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
 import { AccountModel } from '../../../domain/models/account'
 import { Encrypter } from '../../protocols/encrypter'
-import { promises } from 'fs'
-import { resolve } from 'dns'
+
 export class DbAddAccount implements AddAccount {
     private readonly encrypter: Encrypter
     constructor (encrypter: Encrypter) {
