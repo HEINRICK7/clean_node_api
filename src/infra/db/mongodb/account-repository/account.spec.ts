5ab062b269c374dde22b227eaf0466eba22a8014
Carlos Henrique <heinrickcostta@gmail.com>
2020-11-23 15:12:21 -0300

    feat: modified

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,28 @@
+import { MongoHelper} from '../helpers/mongo-helper'
+
+describe('Account Mongo Repository', () => {
+    beforeAll(async()=>{
+       await MongoHelper.connect(process.env.MONGO_URL)
+    })
+
+    afterAll(async()=> {
+        await MongoHelper.disconnect()
+    })
+
+    test('should return an account on success', async () => {
+
+        const sut = new AccountMongoRepository()
+        const account =  await sut.add({
+            name: 'any_name',
+            email: 'any_email.com',
+            password: 'any_password'
+        })
+        expect(account).toBeTruthy()
+        expect(account.id).toBeTruthy()
+        expect(account.name).toBe('any_name')
+        expect(account.email).toBe('any_email.com')
+        expect(account.password).toBe('any_password')
+        
+    })
+    
+});
